671803152c88d8f88bacc1d7b4faa2f5dc1918ca
hessikaveh <hesam.kaveh@gmail.com>
2021-04-11 23:58:13 +0200

    set-up the repo

---
+++ server/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import postRoutes from './routes/posts.js';
+
+const app = express();
+
+app.use(express.json({limit: "30mb", extended: true }));
+app.use(express.urlencoded({limit: "30mb", extended: true }));
+app.use(cors());
+
+app.use('/posts', postRoutes);
+
+const CONNECTION_URL = 'mongodb+srv://sam:sam1369@cluster0.kae7d.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const PORT = process.env.PORT || 5000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+mongoose.set('useFindAndModify', false);
